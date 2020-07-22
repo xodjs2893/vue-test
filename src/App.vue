c940af2480050dde937693dd8890b6652741774d
jintaeeon <xodjs2893@naver.com>
2020-07-22 18:47:17 +0900

    vue first app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <HelloWorld />
   </div>
 </template>
 
@@ -24,5 +24,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
 </style>
